d692d6587afdf2ae40279605a80ac4c4c888fdf0
Ishwar-Rana <112081715+Ishwar-Rana@users.noreply.github.com>
2023-04-23 21:17:26 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+@autocaption.on_message(filters.command("start") & filters.incoming)
+async def start(bot, message):
+        await message.reply("<I>I Am Auto Caption Bot Just Add me as a Admin in your channel with edit permission and See Magic</I>\n\n• **support** : @Hollywood_0980\n• **Source** : https://github.com/0AIB/TG-Caption-Bot")
